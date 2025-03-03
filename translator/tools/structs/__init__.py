48052a348d593191ae48369fc00b10a40075cbce
WalterCun <waltercunbustamante@gmail.com>
2025-03-03 13:05:09 -0500

    Rename and restructure JSON utility imports.
    Updated the module to use renamed and restructured utility functions for JSON operations, improving clarity and consistency. Adjusted the `__all__` export to reflect these changes.

--- translator/tools/structs/__init__.py
+++ translator/tools/structs/__init__.py
@@ -1,3 +1,3 @@
-from .json_struct import json_extract_entries
+from .json import serializer_json, deserializar_json, get_content_json_file
 
-__all__ = ['json_extract_entries']
+__all__ = ['serializer_json', 'deserializar_json', 'get_content_json_file']
